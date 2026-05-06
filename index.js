581725c828522a1275e1a9bf646727d393a20a09
Jazmi1908 <63233531+Jazmi1908@users.noreply.github.com>
2026-05-06 15:51:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ client.on('interactionCreate', async (interaction) => {
       return interaction.editReply(`Added to queue: **${track.info.title}**`);
     }
 
-    await player.playTrack({ track: track.encoded });
+    await player.playTrack({ track: { encoded: track.encoded } });
     pausedState.set(interaction.guild.id, false);
 
     const embed = new EmbedBuilder()
